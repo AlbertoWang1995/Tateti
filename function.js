4cc992de5458b8a2420b49f4d6988c4776cf3f07
Alberto Wang <Alberto.Wang1995@gmail.com>
2025-01-23 23:27:48 -0300

    Se modifica funcion para cambiar todas las casillas

--- function.js
+++ function.js
@@ -1,17 +1,34 @@
 
-const contentCelda = document.getElementById('celda1');
+// const contentCelda = document.getElementById('celda1');
 
 
 
-function cambiar () {
-    if(contentCelda.innerHTML=="X"){
-        contentCelda.innerHTML="O";
-    }else{
-        contentCelda.innerHTML="X"
-    }
+// function cambiar () {
+//     if(contentCelda.innerHTML=="X"){
+//         contentCelda.innerHTML="O";
+//     }else{
+//         contentCelda.innerHTML="X"
+//     }
    
-}
+// }
 
-document.getElementById("celda1").onclick=function(){
-    cambiar();
-}
+// document.getElementById("celda1").onclick=function(){
+//     cambiar();
+// }
+
+const cells = document.querySelectorAll('.celda');
+
+cells.forEach((cell) => {
+    cell.addEventListener('click', (event) => {
+      handleClick(event.target);
+    });
+  });
+
+  function handleClick(clickedCell) {
+    // Check if the cell is already filled
+    if (clickedCell.innerHTML === 'X') {
+      clickedCell.innerHTML = 'O'; // Change the text inside the clicked cell
+    } else {
+      clickedCell.innerHTML = 'X';;
+    }
+  }
